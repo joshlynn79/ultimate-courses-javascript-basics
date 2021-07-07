67a2ff327ecda4cda01f5f9d90aae5953dc12011
Josh Lynn <joshlynn79@gmail.com>
2021-07-07 06:19:47 -0600

    12-imperatice-array-iteration.js

--- src/arrays-in-depth/12-imperatice-array-iteration.js
+++ src/arrays-in-depth/12-imperatice-array-iteration.js
@@ -4,4 +4,16 @@ export function imperativeArrayIteration() {
 Imperative Array Iteration
 ===============================================
 `);
+
+  const drinks = ['Pepsi', 'Lemonade', 'Cola'];
+
+  console.log(drinks);
+
+  for (let i = 0; i < drinks.length; i++) {
+    const drink = drinks[i];
+    console.log(drink);
+    if (drink === 'Pepsi') {
+      console.log('Let me pour you a glass');
+    }
+  }
 }
